3eb75b6d61c4db19f965c6b8cbc94dfe32b15946
User Y <usery@ascii>
2020-01-26 13:07:15 -0300

    updated

--- wifi.sh
+++ wifi.sh
@@ -13,15 +13,17 @@
 #
 # A very simple replace (kind of) for network-manager, wicd and even
 # the standar 'networking' script.
-
-# PATH should only include /usr/* if it runs after the mountnfs.sh script
+#
+# Note: wpa_passphrase <ssid> <ssid-password> generates the 'ConfigFile'
+#
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 # Script Variables
 Ifname="wlan0"
 Wsupplicant="wpa_supplicant"
 Dhcpcli="dhclient"
-Thiscript=/etc/init.d/wifi
+Thiscript="/etc/init.d/wifi"
+ConfigFile="/etc/wpa_supplicant/default.conf"
 
 # Load the VERBOSE setting and other rcS variables
 [ -f /etc/default/rcS ] && . /etc/default/rcS
@@ -35,34 +37,33 @@ Thiscript=/etc/init.d/wifi
 #
 assert()
 {
-    ERR_PARAMS=11
-    ERR_ASSERT=13
+  ERR_PARAMS=11
+  ERR_ASSERT=13
 	
-    if [ $# -ne 1 ] && [ $# -ne 2 ]; then
-        echo "MAL"
-        return $ERR_PARAMS
-    fi
+  if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    return $ERR_PARAMS
+  fi
 
-    if [ ! $1 ]; then
-        if [ $# -eq 2 ]; then
-            log_failure_msg "$2"
-        else
-            log_failure_msg "Assert($1) failed"
-        fi
-        exit $ERR_ASSERT
+  if [ ! $1 ]; then
+    if [ $# -eq 2 ]; then
+      log_failure_msg "$2"
+    else
+      log_failure_msg "Assert($1) failed"
     fi
+    exit $ERR_ASSERT
+  fi
 }
 
 set_ifname()
 {
-    # Default wlan interface (to connect) is wlan0
-    # Enviroment variable 'WLAN_IFNAME' can be used
-    # to overwrite the default interface name
-    [ ! -z "$WLAN_IFNAME" ] && Ifname=$WLAN_IFNAME
+  # Default wlan interface (to connect) is wlan0
+  # Enviroment variable 'WLAN_IFNAME' can be used
+  # to overwrite the default interface name
+  [ ! -z "$WLAN_IFNAME" ] && Ifname=$WLAN_IFNAME
 
-    # Checking if interface exist 
-    cat /proc/net/dev | grep -wq $Ifname
-    assert "$? -eq 0" "Interface '$Ifname' unknow"
+  # Checking if interface exist 
+  cat /proc/net/dev | grep -wq $Ifname
+  assert "$? -eq 0" "Interface '$Ifname' unknow"
 }
 
 #
@@ -70,36 +71,41 @@ set_ifname()
 #
 do_start()
 {
-    set_ifname
+  set_ifname
     
-    # Checking if wpa_supplicant is running
-    ps aux | grep -i wpa_supplicant | grep -vq grep
+  # Checking if wpa_supplicant is running
+  ps aux | grep -i wpa_supplicant | grep -vq grep
 
-    if [ $? -eq 0 ]; then
-        log_warning_msg "wpa_supplicant allready running"
-        return 1
-    fi
+  if [ $? -eq 0 ]; then
+    log_warning_msg "wpa_supplicant allready running"
+    return 1
+  fi
 
-    # Checking if wpa_supplicant is working
-    $Wsupplicant -v 1> /dev/null
-    assert "$? -eq 0" "wpa_supplicant missing"
+  # Checking if wpa_supplicant is working
+  $Wsupplicant -v 1> /dev/null
+  assert "$? -eq 0" "wpa_supplicant missing"
 
-    # Checking if dhclient is working
-    $Dhcpcli --version 2> /dev/null
-    assert "$? -eq 0" "dhclient missing"
+  # Checking if dhclient is working
+  $Dhcpcli --version 2> /dev/null
+  assert "$? -eq 0" "dhclient missing"
 
-    # launch it
-    $Wsupplicant -i $Ifname -c /etc/wpa_supplicant.conf -B
-    if [ $? -ne 0 ]; then
-		log_failure_msg "Error starting wifi.sh"
-		return 2
-    fi
-    dhclient $Ifname
-    if [ $? -ne 0 ]; then
-		log_failure_msg "dhclient error"
-		return 3
-    fi
-    log_success_msg "Wifi Started"
+  # Checking if default config file exists
+  test -f "$ConfigFile"
+  assert "$? -eq 0" "Config file '$ConfigFile' missing"
+
+  # launch it
+  $Wsupplicant -i $Ifname -c "$ConfigFile" -B
+  if [ $? -ne 0 ]; then
+    log_failure_msg "Error starting wifi.sh"
+    return 2
+  fi
+    
+  dhclient $Ifname
+  if [ $? -ne 0 ]; then
+    log_failure_msg "dhclient error"
+    return 3
+  fi
+  log_success_msg "Wifi Started"
 }
 
 #
@@ -107,9 +113,14 @@ do_start()
 #
 do_stop()
 {
-    $Dhcpcli -r $Ifname
-    pkill wpa_supplicant
-    log_success_msg "Wifi Stopped"
+  ps aux | grep -i wpa_supplicant | grep -vq grep
+  if [ $? -ne 0 ]; then
+    log_warning_msg "Wifi not running"
+    return 1
+  fi
+  $Dhcpcli -r $Ifname
+  pkill wpa_supplicant
+  log_success_msg "Wifi Stopped"
 }
 
 #
@@ -123,17 +134,17 @@ do_reload()
 
 do_status()
 {
-	ps aux | grep -i wpa_supplicant | grep -vq grep
-    if [ $? -ne 0 ]; then
-        log_warning_msg "Wifi not running"
-        return 1
-    fi
-    ps axo command | grep dhcl | grep -wq $Ifname
-	if [ $? -ne 0 ]; then
-        log_warning_msg "Wifi not running"
-        return 1
-    fi
-    log_success_msg "Wifi Running"
+  ps aux | grep -i wpa_supplicant | grep -vq grep
+  if [ $? -ne 0 ]; then
+    log_warning_msg "Wifi not running"
+    return 1
+  fi
+  ps axo command | grep dhcl | grep -wq $Ifname
+  if [ $? -ne 0 ]; then
+    log_warning_msg "Wifi not running"
+    return 1
+  fi
+  log_success_msg "Wifi Running"
 }
 
 case "$1" in
@@ -147,12 +158,11 @@ case "$1" in
     do_reload
     ;;
   status)
-	do_status
-	;;
+    do_status
+    ;;
   *)
     echo "Usage: $Thiscript {start|stop|restart|force-reload|status}" >&2
     exit 3
     ;;
 esac
-
 :
